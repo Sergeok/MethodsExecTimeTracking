c5425380956341fefc3361eb497a54cd50732df0
Sumin Sergey <mail.sergey.sumin@gmail.com>
2024-04-14 05:13:06 +0700

    Передача размера пула в конфиги

--- src/main/java/ok/serge/config/ExecutorConfig.java
+++ src/main/java/ok/serge/config/ExecutorConfig.java
@@ -1,6 +1,7 @@
 package ok.serge.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,20 +13,20 @@ public class ExecutorConfig {
 
     @Bean
     @Qualifier(value = "statisticPersistExecutor")
-    public Executor statisticPersistExecutor() {
+    public Executor statisticPersistExecutor(@Value("${statistic.executor.pool.size}") int poolSize) {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setThreadNamePrefix("stat-thread-");
-        threadPoolTaskExecutor.setCorePoolSize(2);
+        threadPoolTaskExecutor.setCorePoolSize(poolSize);
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
     }
 
     @Bean
     @Qualifier(value = "workerExecutor")
-    public Executor workerExecutor() {
+    public Executor workerExecutor(@Value("${worker.executor.pool.size}") int poolSize) {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setThreadNamePrefix("work-thread-");
-        threadPoolTaskExecutor.setCorePoolSize(2);
+        threadPoolTaskExecutor.setCorePoolSize(poolSize);
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
     }
